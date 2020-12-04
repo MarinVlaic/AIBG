4b1469618d74684d438f8ebeb08fdbadff2d4ce3
Marin <marin.vlaic9@gmail.com>
2020-12-04 21:05:11 +0100

    c1

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 import requests
 
-game_id = None
-player_id = None
+game_id = int(input("Game id: "))
+player_id = int(input("Player id: "))
+
+train = bool(input("Is train: "))
+url = 'http://localhost:9080/' if not train else 'http://localhost:9080/train'
+
+
+
+
