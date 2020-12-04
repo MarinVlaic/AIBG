d27cc8d213f9fc4e37a147748419d28e03e4e0bb
MikiFER <ivan.mikulic.97@gmail.com>
2020-12-04 23:08:40 +0100

    Implemented move action

--- src/actions/move.py
+++ src/actions/move.py
@@ -1,2 +1,14 @@
-class Move:
-    pass
+from actions.action import Action
+from playerprofile import PlayerProfile
+from typing import Dict
+from mapfeatures.intersection import Intersection
+
+class Move(Action):
+
+    def __init__(self, intersection_id : int):
+        self.intersection_id = intersection_id
+
+    def apply_move(self, player_profile: PlayerProfile, intersections: Dict[int, Intersection]):
+        player_profile.current_builder_intersection_position = intersections[self.intersection_id]
+        player_profile.resources["SHEEP"] -= 50
+        player_profile.resources["WHEAT"] -= 50
