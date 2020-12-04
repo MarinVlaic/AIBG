ead2984d1deb67429aa6dd8312ca2f7e090da676
MikiFER <ivan.mikulic.97@gmail.com>
2020-12-04 22:33:07 +0100

    Added builder position to playerprofile.py

--- src/playerprofile.py
+++ src/playerprofile.py
@@ -2,6 +2,7 @@ class PlayerProfile:
     def __init__(self):
         self.cities = []
         self.resources = {"SHEEP": 0, "WOOD": 0, "WHEAT": 0, "CLAY": 0, "IRON": 0}
+        self.current_builder_intersection_position = None
 
     def get_score(self) -> int:
         # TODO: Calculate player score (sum of city levels)
